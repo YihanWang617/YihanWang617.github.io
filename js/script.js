cd0b566dde651b8848038a569b4e801f62a4974a
王艺涵 <wangyihan617@gmail.com>
2020-06-19 13:39:13 +0800

    update umeng

--- js/script.js
+++ js/script.js
@@ -5,4 +5,6 @@ $(document).ready(function() {
   })
 });
 
-var cnzz_protocol = (("https:" == document.location.protocol) ? "https://" : "http://");document.write(unescape("%3Cspan id='cnzz_stat_icon_1278055084'%3E%3C/span%3E%3Cscript src='" + cnzz_protocol + "s4.cnzz.com/z_stat.php%3Fid%3D1278055084%26show%3Dpic1' type='text/javascript'%3E%3C/script%3E"));
+var cnzz_protocol = (("https:" == document.location.protocol) ? "https://" : "http://");
+document.write(unescape("%3Cspan id='cnzz_stat_icon_1279005786'%3E%3C/span%3E%3Cscript src='" + cnzz_protocol + "s9.cnzz.com/z_stat.php%3Fid%3D1279005786%26show%3Dpic' type='text/javascript'%3E%3C/script%3E"));
+// document.write(unescape("%3Cspan id='cnzz_stat_icon_1279005786'%3E%3C/span%3E%3Cscript src='" + cnzz_protocol + "s4.cnzz.com/z_stat.php%3Fid%3D1279005786%26show%3Dpic1' type='text/javascript'%3E%3C/script%3E"));
